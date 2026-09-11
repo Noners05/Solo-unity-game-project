8dd812f5e5be6bd51ec635b596c948ef801a4ef2
Noners05 <noners02@icloud.com>
2026-09-11 10:24:04 -0700

    almost finished movement code

--- Bryan_Unity Project/Assets/Scripts/PlayerControler.cs
+++ Bryan_Unity Project/Assets/Scripts/PlayerControler.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jumphight = 10f;
+
+    PlayerInput playerinput;
+    Rigidbody rb;
+
+    Vector3 moveInput;
+
+   
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //initializing component data
+        rb = GetComponent<Rigidbody>();
+        playerinput = GetComponent<PlayerInput>();
+
+        //Setting up new move vectors
+        moveInput = new Vector2();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 tempMovement = rb.linearVelocity;
+
+        rb.linearVelocity = (moveInput x * speed) + (moveinput y * speed) + (moveInput z * speed)
     }
+}
+public void Move(InputAction CallBackContext context)
+{
+ 
 }
